070d54d8aca1a44b90414ee63d6450882838ee0e
Pedro Neto <netopedroo117@gmail.com>
2025-07-06 22:18:24 -0300

    setup do knex

--- class/02-api-rest-nodejs/src/server.ts
+++ class/02-api-rest-nodejs/src/server.ts
@@ -1,9 +1,10 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const test = knex
 })
 
 app
